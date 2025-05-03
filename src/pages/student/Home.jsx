0a29eb28c420919489ca3dc88b34be2cac6f770a
Sankalp-Pradhan <sankalppradhan1906@gmail.com>
2025-05-03 23:27:46 +0530

    navbar

---
+++ src/pages/student/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero from '../../components/student/hero'
+
+const Home = () => {
+  return (
+    <div className='flex flex-col items-center space-y-7'>
+      <Hero/>
+    </div>
+  )
+}
+
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ["./src/**/*.{html,js,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontSize :{
+        'course-detail-heading-small ' : ['26px' , '36px'],
+
+      }
+    },
+  },
+  plugins: [],
+}
