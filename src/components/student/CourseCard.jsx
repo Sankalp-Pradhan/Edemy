80a88c293c97ed65ce39fb8ca4ce3cc2ea72a74a
Sankalp-Pradhan <sankalppradhan1906@gmail.com>
2025-05-04 01:15:44 +0530

    searchBar,Companies,course list done

--- src/components/student/CourseCard.jsx
+++ src/components/student/CourseCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CourseCard = () => {
   return (
     <div>
-        <h1>courseacard</h1>
+
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ export default {
     extend: {
       fontSize :{
         'course-detail-heading-small ' : ['26px' , '36px'],
+        "home-heading-small" : ['36px', '44px'],
+        'course-detail-heading-large' : ['28px','34px'],
+        'home-Heading-large' :['48px','56px' ],
+        'default' : ['15px', '21px']
 
       }
     },
